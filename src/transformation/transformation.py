163c8572664d4727ddb3a7dbddd1080274455799
Debanjan69 <84197006+Debanjan69@users.noreply.github.com>
2026-04-08 01:19:51 +0530

    Update transformation.py

--- src/transformation/transformation.py
+++ src/transformation/transformation.py
@@ -1,1 +1,24 @@
+from src.transformation.bronze_to_silver import transform_to_silver
+from src.transformation.silver_to_gold import transform_to_gold
+from src.utils.logger import get_logger
 
+logger = get_logger()
+
+def run_pipeline():
+    try:
+        logger.info("Starting Silver Transformation...")
+        silver_df = transform_to_silver()
+
+        logger.info("Silver Layer Completed")
+
+        logger.info("Starting Gold Transformation...")
+        gold_df = transform_to_gold()
+
+        logger.info("Gold Layer Completed")
+
+    except Exception as e:
+        logger.error(f"Pipeline Failed: {str(e)}")
+        raise
+
+if __name__ == "__main__":
+    run_pipeline()
