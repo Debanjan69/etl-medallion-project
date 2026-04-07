d33ca7a75ba6752ebb61eca80cdf721ea2540b7e
Debanjan69 <84197006+Debanjan69@users.noreply.github.com>
2026-04-08 00:30:36 +0530

    Update ingest.py

--- src/ingestion/ingest.py
+++ src/ingestion/ingest.py
@@ -1,34 +1,25 @@
 from src.utils.spark_session import get_spark_session
-from src.config_reader import read_config,get_input_files
-from delta.tables import DeltaTable
-import os
+from src.utils.helpers import standardize_columns
+from src.config_reader import read_config
 
 def ingest_data():
-  spark = get_spark_session()
-  config = read_config()
-  raw_path = config["paths"]["raw"]
-  bronze_path = config["paths"]["bronze"]
-  input_files = get_input_files()
+    spark = get_spark_session()
+    config = read_config()
 
-  for file_name in input_files:
-    file_path = os.path.join(raw_path,file_name)
-    table_name = os.path.splitext(file_name)[0]
-    table_name_clean = re.sub(r'\d+$', '', table_name)
-    df = spark.read.format("csv").option("header",True).option("inferSchema",True).load(file_path)
-    #standardize columns
+    df = spark.read.csv(
+        config["paths"]["raw"] + config["files"]["input_file"],
+        header=True,
+        inferSchema=True
+    )
+
     df = standardize_columns(df)
-    #write to Bronze layer with table-specific folder
-    full_bronze_path = os.path.join(bronze_path,table_name_clean)
-    if DeltaTable.isDeltaTable(spark,full_bronze_path):
-      bronze_table = DeltaTable.forPath(spark,full_bronze_path)
-      bronze_table.alias("t").merge(df.alias("s"),"t.ingestion_date = s.ingestion_date").whenNotMatchedInsertAll().execute()
-    else:
-      df.write.format("delta").mode("overwrite").save(full_bronze_path)
-      
-    print(f"Ingested File:{file_name} -> Bronze:{full_bronze_path}")
+  
+    df.write.format("delta") \
+        .mode("overwrite") \
+        .save(config["paths"]["bronze"])
+
     
-  print("All Files Ingested Successfully")
 
 if __name__ == "__main__":
-  ingest_data()
+    ingest_data()
       
