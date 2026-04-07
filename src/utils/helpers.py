fea812913b3755de169d62092fe2dbc95a05901e
Debanjan69 <84197006+Debanjan69@users.noreply.github.com>
2026-04-07 22:41:25 +0530

    Update helpers.py
    modified the standardizations

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,5 +1,14 @@
+from pyspark.sql.functions import current_date
+
 def standardize_columns(df):
-  for col in df.columns:
-    df = df.withColumnRenamed(col,col.lower())
-  return df
+    # 1️⃣ Standardize column names
+    for col in df.columns:
+        df = df.withColumnRenamed(col, col.lower())
     
+    # 2️⃣ Remove duplicate rows
+    df = df.dropDuplicates()
+    
+    # 3️⃣ Add ingestion date column
+    df = df.withColumn("ingestion_date", current_date())
+    
+    return df
