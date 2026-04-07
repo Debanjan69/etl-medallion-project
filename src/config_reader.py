d23630050e67a43ec86640057bb89dd01411f252
Debanjan69 <84197006+Debanjan69@users.noreply.github.com>
2026-04-07 22:05:59 +0530

    Update config_reader.py
    added a user defined function get_input_files to fetch the file names

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,4 +1,8 @@
 import yaml
-def read_config(path):
+def read_config(path="config/config.yaml"):
   with open(path,'r') as file:
     return yaml.safe_load(file)
+
+def get_input_files():
+  config = read_config()
+  return config.get("input_file",[])
